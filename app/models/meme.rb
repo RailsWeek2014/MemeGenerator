580fd01843a7a741fb77186c01f1bbe5d0c55d75
Nils <nils.lack@googlemail.com>
2014-09-10 15:17:54 +0000

    Tag system anfang

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -2,11 +2,13 @@ class Meme < ActiveRecord::Base
 
     acts_as_commentable
     acts_as_likeable
+    acts_as_taggable
 
     attr_accessor :textoben
     attr_accessor :textunten
     attr_accessor :template_id
     attr_accessor :comment
+    attr_accessor :tag_list
     validates :title, length: { in: 1..100 }
     validates :description, length: { in: 1..200 }
     validates :textoben, length: { in: 1..120 }
